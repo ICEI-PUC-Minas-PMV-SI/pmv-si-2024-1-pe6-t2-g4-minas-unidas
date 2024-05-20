aef00bd54159357ba0b5c6860c0a3488537078ae
Carolina Moura <crvo.carolina@gmail.com>
2024-05-20 16:01:01 -0300

    Revise login script to handle errors

--- src/frontend/js/login.js
+++ src/frontend/js/login.js
@@ -10,12 +10,31 @@ document.getElementById("loginButton").addEventListener("click", function () {
         },
         body: JSON.stringify({ email: email, password: password }),
     })
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao autenticar');
+            }
+            return response.json();
+        })
         .then(data => {
             console.log(data);
-            window.location.href = 'http://localhost:3000/dashboard-usuarios.html';
+            // Verifica se o token está presente na resposta
+            if (data.token) {
+                window.location.href = 'http://localhost:3000/dashboard-usuarios.html';
+            } else {
+                console.error('Token não encontrado na resposta');
+                showErrorModal("Token não encontrado na resposta");
+            }
         })
         .catch((error) => {
             console.error('Erro ao fazer login:', error);
+            showErrorModal("Erro ao fazer login. Por favor, tente novamente.");
         });
 });
+
+function showErrorModal(message) {
+    var errorMessageElement = document.getElementById("errorMessage");
+    errorMessageElement.innerText = message;
+    var errorModal = new bootstrap.Modal(document.getElementById('errorModal'));
+    errorModal.show();
+}
